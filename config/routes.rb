a34c8acfeeb38c1a6b53e3a84a80cfe3256ec886
Yury Kotov <bairkan@gmail.com>
2013-02-24 13:52:22 +0400

    Commented out routes for authorization and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
   root :to => 'exercises#index'
 
-  resources :users do
-    get :activate, on: :member
-  end
+  #resources :users do
+  #  get :activate, on: :member
+  #end
 
-  resources :user_sessions
-  resources :password_resets
+  #resources :user_sessions
+  #resources :password_resets
 
-  match 'login' => 'user_sessions#new', :as => :login
-  match 'logout' => 'user_sessions#destroy', :as => :logout
-  get 'signup' => 'users#new', :as => :signup
+  #match 'login' => 'user_sessions#new', :as => :login
+  #match 'logout' => 'user_sessions#destroy', :as => :logout
+  #get 'signup' => 'users#new', :as => :signup
 
-  resource :oauth do
-    get :callback
-  end
-  match 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
+  #resource :oauth do
+  #  get :callback
+  #end
+  #match 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
 
   namespace :admin do
     resources :sentences
