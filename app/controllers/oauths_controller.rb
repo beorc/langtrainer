a8b9878b8069bcd1b9d4c1238c990becc8c3aad7
Yury Kotov <bairkan@gmail.com>
2013-03-03 01:15:19 +0400

    Localized flash messages while sign in through oauth

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -12,17 +12,17 @@ class OauthsController < ApplicationController
     provider = params[:provider]
     begin
     if @user = login_from(provider)
-      redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
+      redirect_to root_path, :notice => t('flash.oauths.logged_in', provider: provider.titleize)
     else
       begin
         @user = create_from(provider)
         reset_session # protect from session fixation attack
         auto_login(@user)
-        redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
+        redirect_to root_path, :notice => t('flash.oauths.logged_in', provider: provider.titleize)
       rescue => e
         logger.error "==Oauth Error== #{e.message}"
         logger.error e.backtrace.join("\n")
-        redirect_to root_path, :alert => "Failed to login from #{provider.titleize}!"
+        redirect_to root_path, :alert => t('flash.oauths.logged_in', provider: provider.titleize)
       end
     end
     rescue ::OAuth2::Error => e
