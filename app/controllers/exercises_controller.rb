bdae0c281deaf4ec73f35a03e7b85ca067f55fb3
Yury Kotov <bairkan@gmail.com>
2013-03-15 23:07:33 +0400

    Hints existence checking corrected

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -7,7 +7,7 @@ class ExercisesController < ApplicationController
     @language = Language.find params[:language_id]
 
     hint = "exercises/#{@language.slug}/hint_#{@exercise.slug}"
-    hint_path = Rails.root.join('app', 'views', "#{hint_path}.#{I18n.locale}.html.slim")
-    @hint = hint_path if File.exists?(hint_path)
+    hint_path = Rails.root.join('app', 'views', 'exercises', @language.slug.to_s, "_hint_#{@exercise.slug}.#{I18n.locale}.html.slim").to_s
+    @hint = hint if File.exists?(hint_path)
   end
 end
