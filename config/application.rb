b9158a572efe30458fbaee564c77382a2e646e5d
Yury Kotov <bairkan@gmail.com>
2013-03-17 10:27:47 +0400

    Turned of can for assets for a while

--- config/application.rb
+++ config/application.rb
@@ -118,7 +118,7 @@ module Langtrainer
       end if File.exists?(env_file)
     end
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    #config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    #config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
