a79fb9f5c9fab83d97ffb39fa2fe14ab657a2f37
Yury Kotov <bairkan@gmail.com>
2013-03-30 14:02:10 +0400

    Exercise: checking if sentences on selected language present

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -15,7 +15,7 @@ class Sentence < ActiveRecord::Base
   scope :order_by_position, -> { order('position ASC') }
   scope :atoms, -> { where(atom: true) }
   scope :compound, -> { where('atom IS NULL OR atom = false') }
-  scope :with_language, ->(language) { where('? IS NOT NULL', language.slug) }
+  scope :with_language, ->(language) { where("#{language.slug} IS NOT NULL") }
   def self.for_user(user)
     where('user_id IS NULL OR user_id = ?', user.id) - joins(:corrections).where('corrections_sentences.user_id = ?', user.id)
   end
