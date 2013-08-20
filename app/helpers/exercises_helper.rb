84bbf23412e39d033604b99abeca2ab69e4e7add
Yury Kotov <bairkan@gmail.com>
2013-08-20 12:25:11 +0400

    Esp hints markup refactoring, fixed exercises paths

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,7 +1,7 @@
 module ExercisesHelper
   def render_exercises_select
     options = exercises_list_options(include_all: false) do |exercise|
-      language_exercise_path(@language.slug, id: exercise.slug)
+      language_exercise_path(@language.slug, id: exercise.id)
     end
     select_tag 'exercises', options
   end
